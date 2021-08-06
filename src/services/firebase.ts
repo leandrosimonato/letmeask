88b4f42cdcfebb19e354dd9f2bb979a0a02ef232
leandrosimonato <leandrosimonato@gmail.com>
2021-08-06 17:28:49 -0300

    @types/react-router-dom -D, para estabilizar o DOM no React.
    Signed-off-by: leandrosimonato <leandrosimonato@gmail.com>

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,7 +1,7 @@
-import firebase from "firebase/app";
+import firebase from 'firebase/app';
 
-import 'firebase/auth'
-import 'firebase/database'
+import 'firebase/auth';
+import 'firebase/database';
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
@@ -13,8 +13,9 @@ const firebaseConfig = {
   appId: process.env.REACT_APP_APP_ID
 };
 
+firebase.initializeApp(firebaseConfig);
 
-firebase.initializeApp(firebaseConfig)
+const auth = firebase.auth();
+const database = firebase.database();
 
-export const auth = firebase.auth()
-export const database = firebase.database()
+export { firebase, auth, database }
