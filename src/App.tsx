adff4eca1f4bb5c3981de849f4f918a2d0daf4ef
leandrosimonato <leandrosimonato@gmail.com>
2021-08-10 17:35:37 -0300

    Refatorando funções page Room.
    Signed-off-by: leandrosimonato <leandrosimonato@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,4 @@
-import { BrowserRouter, Route, Switch, Link } from 'react-router-dom'
-import usePersistedState from './utils/usePersistedState'
-
-import { ThemeProvider, DefaultTheme } from 'styled-components';
-import GlobalStyle from './styles/global'
-import Header from './components/Header';
-import light from './styles/themes/light'
-import dark from './styles/themes/dark'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
@@ -14,29 +7,16 @@ import { AdminRoom } from './pages/AdminRoom';
 
 import { AuthContextProvider } from './contexts/AuthContext'
 
-
-const App = () => {
-  const [theme, setTheme] = usePersistedState<DefaultTheme>('theme', light)
-
-  const toggleTheme = () => {
-    setTheme(theme.title === 'light' ? dark : light);
-  };
-
-
+function App() {
   return (
     <BrowserRouter>
       <AuthContextProvider>
         <Switch>
-          <ThemeProvider theme={theme}>
-            <GlobalStyle />
-            <Header toogleTheme={toggleTheme} />
-            <Route path="/" exact component={Home} />
-            <Route path="/rooms/new" component={NewRoom} />
-            <Link to="/url-desejada">Voltar</Link>
-            <Route path="/rooms/:id" component={Room} />
+          <Route path="/" exact component={Home} />
+          <Route path="/rooms/new" component={NewRoom} />
+          <Route path="/rooms/:id" component={Room} />
 
-            <Route path="/admin/rooms/:id" component={AdminRoom} />
-          </ThemeProvider>
+          <Route path="/admin/rooms/:id" component={AdminRoom} />
         </Switch>
       </AuthContextProvider>
     </BrowserRouter>
