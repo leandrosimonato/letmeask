7e4e472c7e643251c8942c997619cc85dc8378be
leandrosimonato <leandrosimonato@gmail.com>
2021-08-10 11:09:48 -0300

    Instalando temas Claro e escuro.
    Signed-off-by: leandrosimonato <leandrosimonato@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,11 @@
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { BrowserRouter, Route, Switch, Link } from 'react-router-dom'
+import usePersistedState from './utils/usePersistedState'
+
+import { ThemeProvider, DefaultTheme } from 'styled-components';
+import GlobalStyle from './styles/global'
+import Header from './components/Header';
+import light from './styles/themes/light'
+import dark from './styles/themes/dark'
 
 import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
@@ -7,16 +14,29 @@ import { AdminRoom } from './pages/AdminRoom';
 
 import { AuthContextProvider } from './contexts/AuthContext'
 
-function App() {
+
+const App = () => {
+  const [theme, setTheme] = usePersistedState<DefaultTheme>('theme', light)
+
+  const toggleTheme = () => {
+    setTheme(theme.title === 'light' ? dark : light);
+  };
+
+
   return (
     <BrowserRouter>
       <AuthContextProvider>
         <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/rooms/new" component={NewRoom} />
-          <Route path="/rooms/:id" component={Room} />
+          <ThemeProvider theme={theme}>
+            <GlobalStyle />
+            <Header toogleTheme={toggleTheme} />
+            <Route path="/" exact component={Home} />
+            <Route path="/rooms/new" component={NewRoom} />
+            <Link to="/url-desejada">Voltar</Link>
+            <Route path="/rooms/:id" component={Room} />
 
-          <Route path="/admin/rooms/:id" component={AdminRoom} />
+            <Route path="/admin/rooms/:id" component={AdminRoom} />
+          </ThemeProvider>
         </Switch>
       </AuthContextProvider>
     </BrowserRouter>
---
+++ src/styles/themes/dark.ts
@@ -0,0 +1,11 @@
+export default {
+  title: 'dark',
+
+  colors: {
+    primary: '#333',
+    secondary: '#c62e65',
+
+    background: '#222',
+    text: '#fff'
+  },
+}
